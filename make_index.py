0f775577941c830d964086623ef455606c910844
david watson <davidthewatson@gmail.com>
2021-09-21 14:50:25 -0400

    Make title from md filename

--- make_index.py
+++ make_index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import glob
 import os
+import glob
 
 def main():
     os.chdir('src')
@@ -9,11 +9,14 @@ def main():
     html = ''
     lis = []
     for md in glob.glob('*.md'):
-        lis.append(f'<li><a href="{md[:-3]}.html">{md}</a></li>')
+        title_chunks = md[:-3].split('_')
+        title = ' '.join(title_chunks).title()
+        lis.append(f'<li><a href="{md[:-3]}.html">{title}</a></li>')
     for li in lis:
         html += f'<ul>{li}</ul>'
     f = open('index.md', 'w')
     f.writelines(html)
     f.close()
+
 if __name__ == '__main__':
     main()
