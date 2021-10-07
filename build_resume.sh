810c11f23e7c54d6f0e23b3ba4a342144a2a526f
david watson <davidthewatson@gmail.com>
2021-10-07 13:42:31 -0400

    Fix directory

--- build_resume.sh
+++ build_resume.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
  
-while inotifywait -r -e close_write src/
+while inotifywait -r -e close_write resume/
 do
     mkdir -p docs/pdf
     wkhtmltopdf -B 2cm -L 2cm -R 2cm -T 2cm http://localhost:5555/resume/david_watson_resume.html docs/pdf/david_watson_resume.pdf
