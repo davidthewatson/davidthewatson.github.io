4f20e6c687e63fb668976d8f0969da249792d2c1
david watson <davidthewatson@gmail.com>
2022-03-08 10:39:13 -0500

    Delete deprecated junk left over from refactor

--- build_local.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
- 
-while inotifywait -r -e close_write src/
-do
-    #staticjinja build --srcpath=src --outpath=docs
-    ./make_index.py
-    ./md2html.py
-    cp -rf static/* docs/.
-done
